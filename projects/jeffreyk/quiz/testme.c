d79bac0b35a0bcd5a3179a31cf1083b73968f605
Katherine Jeffrey <jeffreyk@oregonstate.edu>
2018-05-06 22:43:51 -0700

    complete test and makefile

--- projects/jeffreyk/quiz/testme.c
+++ projects/jeffreyk/quiz/testme.c
@@ -5,53 +5,63 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
-    return ' ';
+   char options[] = {'[','(','{',' ','a','x','}',')',']'};
+   int i = (rand() % 9);
+   char c = options[i];
+   return c;
 }
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+   int i, o;
+   char input[] = {'r', 'e', 's', 'e', 't'};
+   char s[6];
+   for(i = 0; i < 5; i++){
+      o = (rand() % 5);
+      s[i] = input[o];
+   }
+   s[6] = '\0';
+   char* r = s;
+   return r;
 }
 
 void testme()
 {
-  int tcCount = 0;
-  char *s;
-  char c;
-  int state = 0;
-  while (1)
-  {
-    tcCount++;
-    c = inputChar();
-    s = inputString();
-    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+   int tcCount = 0;
+   char *s;
+   char c;
+   int state = 0;
+   while (1)
+   {
+      tcCount++;
+      c = inputChar();
+      s = inputString();
+      printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
-    if (c == '[' && state == 0) state = 1;
-    if (c == '(' && state == 1) state = 2;
-    if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
-    if (c == 'a' && state == 4) state = 5;
-    if (c == 'x' && state == 5) state = 6;
-    if (c == '}' && state == 6) state = 7;
-    if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
-    if (s[0] == 'r' && s[1] == 'e'
-       && s[2] == 's' && s[3] == 'e'
-       && s[4] == 't' && s[5] == '\0'
-       && state == 9)
-    {
-      printf("error ");
-      exit(200);
-    }
-  }
+      if (c == '[' && state == 0) state = 1;
+      if (c == '(' && state == 1) state = 2;
+      if (c == '{' && state == 2) state = 3;
+      if (c == ' '&& state == 3) state = 4;
+      if (c == 'a' && state == 4) state = 5;
+      if (c == 'x' && state == 5) state = 6;
+      if (c == '}' && state == 6) state = 7;
+      if (c == ')' && state == 7) state = 8;
+      if (c == ']' && state == 8) state = 9;
+      if (s[0] == 'r' && s[1] == 'e'
+	    && s[2] == 's' && s[3] == 'e'
+	    && s[4] == 't' && s[5] == '\0'
+	    && state == 9)
+      {
+	 printf("error ");
+	 exit(200);
+      }
+   }
 }
 
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    testme();
-    return 0;
+   srand(time(NULL));
+   testme();
+   return 0;
 }
