0c9fcce2e8af535451a7f3cc382e5c106237f12d
Katherine Jeffrey <jeffreyk@flip1.engr.oregonstate.edu>
2018-05-09 16:53:01 -0700

    Add files and Makefile command

---
+++ projects/jeffreyk/dominion/randomtestadventurer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "assert.h"
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include "rngs.h"
+
+void adventurerTest(){
+}
+
+int main () {
+   adventurerTest();
+   return 0; 
+}
+
