f4d66a3157c35dbda9f0c911effef6bf3084d4c1
Katherine Jeffrey <jeffreyk@flip3.engr.oregonstate.edu>
2018-05-13 23:36:17 -0700

    Fix bugs

--- projects/jeffreyk/dominion/randomtestadventurer.c
+++ projects/jeffreyk/dominion/randomtestadventurer.c
@@ -1,15 +1,68 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "assert.h"
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include "rngs.h"
 
-void adventurerTest(){
+int adventurerTest(){
+   //MAX_HAND and MAX_DECK are 500
+   //MAX_PLAYERS is 4
+   int r, i, j, result, expected;
+   int z = 0;
+   int numPlayers = 2;
+   int player1 = 0;
+   int player2 = 1;
+   int seed = 99;
+   int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+   int drawnTreasure = 0;
+   struct gameState G;
+   memset(&G, 23, sizeof(struct gameState));
+   r = initializeGame(numPlayers, k, seed, &G);
+   
+   G.deckCount[player1] = 10;
+   int temphand[MAX_HAND];
+   int cards[10] = {adventurer, gardens, smithy, gold, silver, copper, mine, feast, embargo, minion};
+   for(int d = 0; d < 10; d++){
+      G.deck[player1][d] = cards[rand() % 10];
+   }
+   int treasure1 = -1;
+   int treasure2 = -1;
+   for(int c = 9; c > -1; c--){
+      if(G.deck[player1][c] == copper || G.deck[player1][c] == gold || G.deck[player1][c] == silver){
+	 if(treasure1 == -1){
+	    treasure1 = c;
+	 }
+	 else if(treasure2 == -1 && treasure1 != -1){
+	    treasure2 = c;
+	 }
+	 else{
+	    return 1;
+	 }
+      }
+   }
+   //adventurerAction(drawnTreasure, &G, player1, cardDrawn, player2, temphand, z);
+   cardEffect(adventurer, 0, 0, z, &G, cardDrawn, 0);
+   if(treasure2 <= 0){
+      expected = 0;
+   }
+   else{
+      expected = treasure2 - 1;
+   }
+   result = G.handCount[player1];
+   return assertInt(result, expected);
 }
 
-int main () {
-   adventurerTest();
+int main() {
+   srand(time(NULL));
+   int test = 0;
+   int i;
+   for(i = 0; i < 298; i++){
+      test += adventurerTest();
+   }
+   printf("FAILS: %d\n", test);
    return 0; 
 }
 
--- projects/jeffreyk/dominion/randomtestcard1.c
+++ projects/jeffreyk/dominion/randomtestcard1.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "assert.h"
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include "rngs.h"
 
-void smithyTest(){
-   int i, result, s = 99, numPlayers = 2;
+int smithyTest(){
+   int result, expected = 3, s = 99;
+   int numPlayers = (rand() % 2) + 1;
    struct gameState testgame;
    int k[10] = {adventurer, feast, mine, village, baron, gardens, embargo, tribute, minion, council_room};
-   memset(&testgame, 23, sizeof(struct gameState));
+   memset(&testgame, '\0', sizeof(struct gameState));
    initializeGame(numPlayers, k, s, &testgame);
 
-   int player1 = 0;
-   int testresult, expectedresult;
-   for(i = 0; i < testgame[player1]; i++){
-      printf("");
-   }
-   smithyAction(player1, &testgame, testgame.handCount[player1][i]);
-   for(i = 0; i < testgame.handCount[player1]; i++){
-      printf("");
-   }
+   int player1 = (rand() % 2) + 1;
+   int pos = (rand() % 9) + 1;
+   cardEffect(smithy, 0, 0, 0, &testgame, pos, 0);
+   result = testgame.handCount[player1];
+   if(result == expected)
+      return 0;
+   return 1;
 }
 
 int main () {
-   smithyTest();
+   srand(time(NULL));
+   int test = 0;
+   int i;
+   for(i = 0; i < 220; i++){
+      test += smithyTest();
+   }
+   printf("FAILS: %d\n", test);
    return 0; 
 }
 
