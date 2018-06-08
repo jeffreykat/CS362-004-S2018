ff1b144b0612d9ee386c32fe20c22f7573e862f9
chews93319 <chews@oregonstate.edu>
2018-06-07 23:15:09 -0700

    Update UrlValidatorTest with Adaptation of ManualTests
    During my review, I found the following site, which helped:
    http://www.vogella.com/tutorials/JUnit/article.html
    
    Using this, I adapted the 'Manual Tests' from Katherine's code into this JUnit test case.
    Perhaps this could be a start of a JUnit 'Unit Test' method....?

--- projects/jeffreyk/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/jeffreyk/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,9 @@
 
 
 import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import static org.junit.runners.Parameterized.*;
+import org.junit.Test;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -11,17 +14,42 @@ import junit.framework.TestCase;
 
 
 public class UrlValidatorTest extends TestCase {
-
+	static String[] valid = {"http://www.google.com",
+			"ftp://123.32.0.193",
+			"0.0.0.0/t123",
+			"255.com/test1",
+			"https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top",
+			"ldap://[2001:db8::7]/c=GB?objectClass?one",
+			"mailto:John.Doe@example.com",
+			"news:comp.infosystems.www.servers.unix",
+			"tel:+1-816-555-1212",
+			"telnet://192.0.2.16:80/",
+			"urn:oasis:names:specification:docbook:dtd:xml:4.1.2"};
+	static String[] invalid = {"http:///w.google",
+			"://1.2.3.4.5:80",
+			"http://255.com:-1/test1",
+			"ftp://go.com:65a/../",
+			":80/t23",
+			""};
+	
 
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
    
-   
+   @Test
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+	   UrlValidator urlVal = new UrlValidator(null, null, 0);
+	   
+	   for(int i = 0; i < valid.length; i++) {
+		   assertEquals(String.format("Fail valid case:%s",valid[i]),urlVal.isValid(valid[i]),true);
+	   }
+	   
+	   for(int i = 0; i < invalid.length; i++) {
+		   assertEquals(String.format("Fail invalid case:%s",invalid[i]),urlVal.isValid(invalid[i]),false);
+	   }
 	   
    }
    
