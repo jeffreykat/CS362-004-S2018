92cb8d2c6a1d3ed42eb2c03021046894f89ad8f4
Katherine Jeffrey <jeffreyk@oregonstate.edu>
2018-06-09 18:35:15 -0700

    Add programming tests

--- projects/jeffreyk/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/jeffreyk/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -2,14 +2,11 @@ import junit.framework.TestCase;
 import static org.junit.Assert.*;
 import static org.junit.runners.Parameterized.*;
 import org.junit.Test;
+import java.util.Random;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
-
-
-
-
 
 public class UrlValidatorTest extends TestCase {
 	static String[] valid = {"http://www.google.com",
@@ -29,13 +26,16 @@ public class UrlValidatorTest extends TestCase {
 			"ftp://go.com:65a/../",
 			":80/t23",
 			""};
+	static String[] schemes = {"http://", "ftp:", "", "http/", "ftp", "://", "h3t"};
+	static String[] authorities = {"www.go.com", "1.2.3.4.", "www.google.com", "", "193.36.128.43", "err", "john.doe@www.example.com"};
+	static String[] ports = {":80", ":98765", "", ":aa", ":3000", ":-3", ":0"};
+	static String[] paths = {"/test", "/..", "/path/file", "/../", "/file/", "//test//", ""};
+	static String[] queries = {"?action=view","err","","","","?err","?action=edit&mode=up"};
 	
-
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
-   
    @Test
    public void testManualTest()
    {
@@ -50,7 +50,6 @@ public class UrlValidatorTest extends TestCase {
 	   }
 	   
    }
-   
    
    public void testYourFirstPartition()
    {
@@ -147,9 +146,44 @@ public class UrlValidatorTest extends TestCase {
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
-
+	   UrlValidator urltest = new UrlValidator(null, null, 0);
+	   String url;
+	   for(int i = 0; i < schemes.length; i++) {
+		   if(i % 2 == 0) {
+			   System.out.println(schemes[i]);
+			   assertTrue(urltest.isValidScheme(schemes[i]));
+			   System.out.println(authorities[i]);
+			   assertTrue(urltest.isValidAuthority(authorities[i]));
+			   System.out.println(paths[i]);
+			   assertTrue(urltest.isValidPath(paths[i]));
+			   System.out.println(queries[i]);
+			   assertTrue(urltest.isValidQuery(queries[i]));
+		   }
+		   else {
+			   System.out.println(schemes[i]);
+			   assertFalse(urltest.isValidScheme(schemes[i]));
+			   System.out.println(authorities[i]);
+			   assertFalse(urltest.isValidAuthority(authorities[i]));
+			   System.out.println(paths[i]);
+			   assertFalse(urltest.isValidPath(paths[i]));
+			   System.out.println(queries[i]);
+			   assertFalse(urltest.isValidQuery(queries[i]));
+		   }
+	   }
+	   for(int j = 0; j < 20; j++) {
+		   Random rand = new Random();
+		   int r;
+		   if(j % 2 == 0) {
+			   r = rand.nextInt(valid.length);
+			   System.out.println(valid[r]);
+			   assertTrue(urltest.isValid(valid[r]));
+		   }
+		   else {
+			   r = rand.nextInt(invalid.length);
+			   System.out.println(invalid[r]);
+			   assertFalse(urltest.isValid(invalid[r]));
+		   }
+	   }
    }
    
-
-
 }
